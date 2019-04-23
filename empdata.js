70c3cb6f192b19d5324bed19182c70217a53d93a
anthony <suryajay@seattlu.edu>
2019-04-22 20:05:14 -0700

    fix json file

--- empdata.js
+++ empdata.js
@@ -5,85 +5,85 @@
       "brand": "Hewlett Packard",
       "product_category": "Laptop",
       "price": "$1,499"
-    } 
+    }, 
     {
       "product_name": "NU6900 2160p Smart 4k UHD TV 50inch",
       "brand": "Samsung",
       "product_category": "TV",
       "price": "$329"
-    } 
+    }, 
     {
       "product_name": "XPS 13inch",
       "brand": "Dell",
       "product_category": "Laptop",
       "price": "$1,699"
-    } 
+    }, 
     {
       "product_name": "Note 9",
       "brand": "Samsung",
       "product_category": "Cell Phone",
       "price": "$899"
-    } 
+    }, 
     {
       "product_name": "Switch",
       "brand": "Nintendo",
       "product_category": "Video Games",
       "price": "$299"
-    } 
+    }, 
     {
       "product_name": "Macbook Pro 13inch",
       "brand": "Apple",
       "product_category": "Laptop",
       "price": "$1,399"
-    } 
+    }, 
     {
       "product_name": "UK6190 2160p Smart 4k UHD TV 70inch",
       "brand": "LG",
       "product_category": "TV",
       "price": "$749"
-    } 
+    }, 
     {
       "product_name": "VivoBook 14inch",
       "brand": "Asus",
       "product_category": "Laptop",
       "price": "$459"
-    } 
+    }, 
     {
       "product_name": "iPhone XS 64GB",
       "brand": "Apple",
       "product_category": "Cell Phone",
       "price": "$999"
-    } 
+    }, 
     {
       "product_name": "H8E Series 2160p 4k UHD TV 65inch",
       "brand": "Hisense",
       "product_category": "TV",
       "price": "$549"
-    } 
+    }, 
     {
       "product_name": "Xperia 10 Plus",
       "brand": "Sony",
       "product_category": "Cell Phone",
       "price": "$329"
-    } 
+    }, 
     {
       "product_name": "Inspiron 2-in-1 13inch",
       "brand": "Dell",
       "product_category": "Laptop",
       "price": "$1,199"
-    } 
+    }, 
     {
       "product_name": "PlayStation 4 1TB",
       "brand": "Sony",
       "product_category": "Video Games",
       "price": "$299"
-    } 
+    }, 
     {
       "product_name": "Envy x360 2-in-1 13inch",
       "brand": "Hewlett Packard",
       "product_category": "Laptop",
       "price": "$849"
-    } 
+    }, 
     {
       "product_name": "Xbox One X 1TB",
       "brand": "Microsoft",
